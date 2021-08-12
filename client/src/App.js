404977e585764d5591c2f26c818dfa198919dd63
jiseonBaek <js.coding.place@gmail.com>
2021-08-12 16:17:42 +0900

    style: 초기 스타일링

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,16 @@ import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
 import meal from './images/meal.png';
+import useStyles from './style';
 
 const App = () => {
+    const classes = useStyles();
+
 	return( 
 		<Container maxwidth="lg">
-			<AppBar position="static" color="inherit">
-				<Typography variant="h2" align="center">Memories</Typography>
-				<img src={meal} alt="meal" height="60"/>
+			<AppBar className={classes.appBar} position="static" color="inherit">
+				<Typography className={classes.heading} variant="h2" align="center">Food Tag App</Typography>
+				<img className={classes.image} src={meal} alt="meal" height="60"/>
 			</AppBar>
 			<Grow in>
 				<Container>
---
+++ client/src/style.js
@@ -0,0 +1,25 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles(() => ({
+	appBar: {
+		borderRadius: 100,
+		margin: '40px 0',
+		display: 'flex',
+		flexDirection: 'row',
+		justifyContent: 'center',
+		alignItems: 'center',
+        padding: '12px'
+	      },
+	      heading: {
+        color: '#2E684C',
+        fontWeight: '30'
+	      },
+	      image: {
+        marginLeft: '20px',
+        width: '70px',
+        height: '70px'
+	      },
+	   })
+	      
+
+);
