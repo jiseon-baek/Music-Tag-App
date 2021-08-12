d8334cf61dfe2f374529167fea69f62b09d2d266
jiseonBaek <js.coding.place@gmail.com>
2021-08-12 21:29:42 +0900

    feat&style: 포스트 생성 기능 추가

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,11 +1,48 @@
 import React from 'react';
+import { Card, CardActions, CardMedia, CardContent, Button, Typography } from '@material-ui/core/';
+import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
+import DeleteIcon from '@material-ui/icons/Delete';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
+import moment from 'moment';
+
 import useStyles from './style';
 
 
-const Post = () => {
+const Post = ({ post }) => {
 	const classes = useStyles();
 	return(
-		<h1>POST</h1>
+		<Card className={classes.card}>
+			<CardMedia className={classes.media} image={post.selectedFile} title={post.title}/>
+            <div className={classes.overlay}>
+				<Typography variant="h6">{post.creator}</Typography>
+				<Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
+			</div>
+            <div className={classes.overlay2}>
+				<Button 
+					style={{ color: 'lightgrey'}} 
+					size="small" 
+					onClick={() => {}}>
+					<MoreHorizIcon fontSize="medium" />
+				</Button>
+			</div>
+            <div className={classes.details}>
+				<Typography variant="body2" color="textSecondary">{post.tags.map((tag) => `#️${tag} `)}</Typography>
+			</div>
+            <CardContent>
+				<Typography variant="body" color="textSecondary" component="p">💬 {post.message}</Typography>
+			</CardContent>
+            <CardActions className={classes.cardActions}>
+				<Button size="small" color="" onClick={() => {}}>
+					<ThumbUpAltIcon fontSize="small" />
+					&nbsp; Like &nbsp;
+					{post.likeCount}
+				</Button>
+				<Button size="small" color="" onClick={() => {}}>
+					<DeleteIcon fontSize="small" />
+					Delete
+				</Button>
+			</CardActions>
+		</Card>
 	);
 }
 
