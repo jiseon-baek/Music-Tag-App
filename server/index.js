126fef99b806b09aaf5a5496d13d9d95bc3f3669
jiseonBaek <js.coding.place@gmail.com>
2021-08-12 13:25:35 +0900

    초기세팅: controllers (getPosts, createPosts) 생성

--- server/index.js
+++ server/index.js
@@ -4,17 +4,21 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
 
+import postRoutes from './routes/posts.js';
+
 const app = express();
 dotenv.config();
+
+app.use('/posts', postRoutes);
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-
+const CONNECTION_URL = "mongodb+srv://jiseonbaek:foodtagapp123@cluster0.sngme.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true } )
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true } )
 	.then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
 	.catch((error) =>  console.log(error.message));
 
