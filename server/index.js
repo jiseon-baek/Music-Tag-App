c19f6172c79bd5bfd0fcf465da92c78ffdaa601a
jiseonBaek <js.coding.place@gmail.com>
2021-08-13 18:05:10 +0900

    Test: Deploy 테스트

--- server/index.js
+++ server/index.js
@@ -17,10 +17,15 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = "mongodb+srv://jiseonbaek:foodtagapp123@cluster0.sngme.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+app.get('/', (req, res) => {
+	res.send(
+		'Hello to Music Api'
+	);
+})
+
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true } )
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true } )
 	.then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
 	.catch((error) =>  console.log(error.message));
 
