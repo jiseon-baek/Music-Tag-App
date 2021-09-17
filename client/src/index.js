5aa0e46d716b1d311ec9c4b67f2882d1e67df31f
jiseonBaek <js.coding.place@gmail.com>
2021-09-17 21:48:09 +0900

    chore: logging을 위한 sentry 설치

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 
@@ -10,6 +12,18 @@ import App from './App';
 import './index.css';
 
 const store = createStore(reducers, compose(applyMiddleware(thunk)));
+
+Sentry.init({
+	dsn: "https://e2dae39cc9f04d0a9e7495b72f099410@o1003790.ingest.sentry.io/5964402",
+	integrations: [new Integrations.BrowserTracing()],
+
+	// Set tracesSampleRate to 1.0 to capture 100%
+	// of transactions for performance monitoring.
+	// We recommend adjusting this value in production
+	tracesSampleRate: 0.7,
+});
+
+
 
 ReactDOM.render(
 	<Provider store={store}>
