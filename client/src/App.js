a60ed47adde855729d83c748b85ced49ea054118
jiseonBaek <js.coding.place@gmail.com>
2021-09-25 23:32:14 +0900

    refactor: import

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Container } from '@material-ui/core';
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 
 import PostDetails from './components/PostDetails/PostDetails';
 import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
 import Auth from './components/Auth/Auth';
+
+import { Container } from '@material-ui/core';
 
 
 
