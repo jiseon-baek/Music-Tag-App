380384f52e5640e80bd105d2626c63e81d2abc21
jiseonBaek <js.coding.place@gmail.com>
2021-09-26 13:31:31 +0900

    refactor: prettier setting

--- client/src/App.js
+++ client/src/App.js
@@ -8,30 +8,28 @@ import Auth from './components/Auth/Auth';
 
 import { Container } from '@material-ui/core';
 
-
-
-
-
 const App = () => {
+  const user = JSON.parse(localStorage.getItem('profile'));
 
-    const user = JSON.parse(localStorage.getItem('profile'));
-
-    return (
-        <BrowserRouter>
-            <Container maxwidth="false" style={{ padding: '0' }}>
-                <Navbar />
-                <Switch>
-                    <Route path="/" exact component={() => <Redirect to="/posts" />} />
-                    <Route path="/posts" exact component={Home} />
-                    <Route path="/posts/search" exact component={Home} />
-                    <Route path="/posts/:id" component={PostDetails} />
-                    <Route path="/auth" exact component={() => (!user ? <Auth /> : <Redirect to="/posts" />)} />
-                    <Redirect path="*" to="/" />
-                </Switch>
-            </Container>
-        </BrowserRouter>
-
-    );
-}
+  return (
+    <BrowserRouter>
+      <Container maxwidth='false' style={{ padding: '0' }}>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact component={() => <Redirect to='/posts' />} />
+          <Route path='/posts' exact component={Home} />
+          <Route path='/posts/search' exact component={Home} />
+          <Route path='/posts/:id' component={PostDetails} />
+          <Route
+            path='/auth'
+            exact
+            component={() => (!user ? <Auth /> : <Redirect to='/posts' />)}
+          />
+          <Redirect path='*' to='/' />
+        </Switch>
+      </Container>
+    </BrowserRouter>
+  );
+};
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,8 @@
-import React from 'react'
+import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import * as Sentry from "@sentry/react";
-import { Integrations } from "@sentry/tracing";
+import * as Sentry from '@sentry/react';
+import { Integrations } from '@sentry/tracing';
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 
@@ -14,22 +14,19 @@ import './index.css';
 const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
 Sentry.init({
-	dsn: "https://e2dae39cc9f04d0a9e7495b72f099410@o1003790.ingest.sentry.io/5964402",
-	integrations: [new Integrations.BrowserTracing()],
+  dsn: 'https://e2dae39cc9f04d0a9e7495b72f099410@o1003790.ingest.sentry.io/5964402',
+  integrations: [new Integrations.BrowserTracing()],
 
-	// Set tracesSampleRate to 1.0 to capture 100%
-	// of transactions for performance monitoring.
-	// We recommend adjusting this value in production
-	tracesSampleRate: 0.7,
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 0.7,
 });
 
-
-
 ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
 
-	<Provider store={store}>
-		<App />
-	</Provider>
-
-	,
-	document.getElementById('root'));
+  document.getElementById('root')
+);
